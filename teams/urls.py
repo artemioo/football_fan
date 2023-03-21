35e87d600d141d2823f9afc2b9211c728533e329
artemioo <artemryb1@gmail.com>
2023-03-21 12:55:14 +0200

    add filtration, pagination

--- teams/urls.py
+++ teams/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', main),
     path('team/<slug:slug>/', team, name='team'),
+    path('teams/', Teams.as_view(), name='teams'),
     path('my_teams/', users_fav_teams, name='users_fav_teams'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView
 from teams.models import Team, Match
 from users.models import Profile
 
@@ -14,19 +14,37 @@ def main(request):
 def users_fav_teams(request):
     profile = request.user.profile
     teams = profile.favorite_teams.all()
-    # matches = Match.objects.filter(Q(team_home=teams[1]) | Q(team_away=teams[1]))
-    # matches = []
-    # for team in teams:
-    #     matches.add(Match.objects.filter(team_home=team))
-    #     matches.add(Match.objects.filter(team_away=team))
-    # print(matches)
     context = {'profile': profile, 'teams': teams}
     return render(request, 'teams/users_fav_teams.html', context)
 
 
+class Teams(ListView):
+    model = Team
+    template_name = 'teams/teams_all.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        name = self.request.GET.get('team_name', None)
+        if name:
+            teams = self.model.objects.filter(name__icontains=name)
+        else:
+            teams = self.model.objects.all()
+        return teams
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Teams, self).get_context_data(**kwargs)
+        context['team_name'] = self.request.GET.get('team_name', None)
+
+        return context
+
+
+
 def team(request, slug):
     team = get_object_or_404(Team, slug=slug)
-    matches = Match.objects.filter(Q(team_home=team) | Q(team_away=team))
+    try:
+        matches = Match.objects.filter(Q(team_home=team) | Q(team_away=team))
+    except:
+        matches = None
 
     context = {'team': team, 'matches': matches}
     return render(request, 'teams/team.html', context)
