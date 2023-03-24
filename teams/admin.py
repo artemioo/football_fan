34b8230006cd5483d32215b3140389896c76082b
artemioo <artemryb1@gmail.com>
2023-03-24 11:14:46 +0200

    create TeamAdmin class, add additional info in the admin panel

--- teams/admin.py
+++ teams/admin.py
@@ -4,7 +4,19 @@ from django.contrib.auth.models import User, Permission
 
 from django.contrib.contenttypes.models import ContentType
 
-admin.site.register(Team)
 admin.site.register(Match)
 
 
+class MatchesInline(admin.StackedInline):
+    model = Match
+    fk_name = 'team_home'
+    extra = 1
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_filter = ('country', )
+    search_fields = ('name', 'country')
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [MatchesInline, ]
+    save_as = True
